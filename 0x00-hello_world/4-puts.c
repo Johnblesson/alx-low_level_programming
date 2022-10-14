03cd671069236a373b015030163f50a5623a9ee8
Johnblesson <johnblessonrowe@gmail.om>
2022-10-13 18:26:52 -0700

    4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
-*main- Serves as the entry point for the program
-* 
-*Return: By default returns zero if no errors otherwise returns non-zero value
-*/
+ *main - Printing
+ *
+ *Return: Always 0 (Successful)
+ */
 int main(void)
 {
-           printf("with proper grammar, but the outcome is a piece of art,\n");
-	   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
