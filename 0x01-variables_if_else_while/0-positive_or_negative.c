382ca603bd590aaa7fee8530257adbe03f446137
Johnblesson <johnblessonrowe@gmail.com>
2022-10-14 20:42:33 +0000

    Task 0 commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,23 +11,17 @@
 
 int main(void)
 {
-	int n, last_digit;
+         int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-	printf("Last digit of %d is %d and is ", n, last_digit);
-	if (last_digit > 5)
-	{
-		printf("grater than 5\n");
-	}
-	else if (last_digit  == 0)
-	{
-		printf("0\n");
-	}
-	else if (last_digit < 6 && last_digit != 0)
-	{
-		printf("less than 6 and not 0\n");
-	}
-	return (0);
+	     srand(time(0));
+	     n = rand() - RAND_MAX / 2;
+	     /* your code goes there*/
+		 if (n > 0)
+                 printf("%d is positive\n", n);
+	     else if (n < 0)
+		         printf("%d is negative\n", n);
+	     else
+		         printf("%d is zero \n", n);
+
+	     return (0);
 }
